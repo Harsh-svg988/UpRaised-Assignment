62fb1d2fe3587df58aaaac65c514c9d7473ff4bc
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-01-26 17:01:51 +0530

    Done with basic structure

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { initializeDatabase } from './lib/db.js';
+import authRoute from './route/auth.route.js';
+import gadgetRoute from './route/gadget.route.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoute);
+app.use('/api/gadget', gadgetRoute);
+
+app.listen(PORT, () => {
+  initializeDatabase();
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ backend/src/models/gadget.model.js
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+import { sequelize } from "../lib/db.js";
+
+const Gadget = sequelize.define('Gadget', {
+    id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM(["Available", "Deployed", "Destroyed", "Decommissioned"]),
+        defaultValue: 'Available'
+    },
+    codeName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    successRate: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    decommissionedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    }
+})
+
+export default Gadget;
